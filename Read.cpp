c41891c46d52134cfb796c26c0eae330fc28f1b8
Chenhan Gao <93461099+CharlieGao2009@users.noreply.github.com>
2023-01-18 16:18:07 +0800

    Update Read.cpp

--- Read.cpp
+++ Read.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 char DataRaw[4100];
 uint8_t DataUnsigned8[4100];
+struct F4K{int pos,sz,dfn;} _chunk[40][40];
 int main()
 {
 	ifstream file("first4K.sample",ios::binary|ios::in|ios::ate);
@@ -15,7 +16,20 @@ int main()
 	string s(DataRaw);
 	for (int i=0;i<s.size();i++)
 		DataUnsigned8[i+1]=uint8_t(s[i]);
-	for (int i=1;i<=n;i++)
-		cout<<i<<' '<<+DataUnsigned8[i]<<endl;
+	for (int i=0;i<32;i++)
+	{
+		for (int j=0;j<32;j++)
+		{
+			int id=4*((i&31)+(j&31)*32);
+			_chunk[i][j].pos=(DataUnsigned8[id+1]<<16)+(DataUnsigned8[id+2]<<8)+DataUnsigned8[id+3];
+			_chunk[i][j].sz=DataUnsigned8[id+4];
+		}
+	}
+	for (int i=0;i<32;i++)
+	{
+		for (int j=0;j<32;j++)
+			cout<<i<<' '<<j<<' '<<_chunk[i][j].pos<<' '<<_chunk[i][j].sz<<endl;
+	}
 	return 0;
 }
+//4 * ((x & 31) + (z & 31) * 32)
